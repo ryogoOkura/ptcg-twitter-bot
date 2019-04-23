6e262dc17d46de86f0d5013013e17e617cf6a073
Ryogo Okura <nem1442so@gmail.com>
2019-04-23 19:15:45 +0900

    tweet with cards' image

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, request, redirect, url_for, session
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,66 +8,196 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import random
 import re
-URL='https://www.pokemon-card.com/rules/faq/'
 SERIES_RANGE=4
-import twitter
+URL_PTCG='https://www.pokemon-card.com/'
+URL_PTCG_RULE='rules/faq/'
+import urllib.request
+import json
+from requests_oauthlib import OAuth1Session
+URL_TEXT='https://api.twitter.com/1.1/statuses/update.json'
+URL_TL='https://api.twitter.com/1.1/statuses/home_timeline.json'
+URL_MEDIA='https://upload.twitter.com/1.1/media/upload.json'
 CONSUMER_KEY="0PWItx3y8rT56fHmoksjCD3p8"
 CONSUMER_SECRET="Oc7v7lETeQdhOb2mnCXd3hiSBlgJ4DlYSADt7uZlBFSo9IRpbB"
 TOKEN="1103483478723584000-WCkBL5Hjf5bB6rZlsbgrSy2LrPLj8I"
 TOKEN_SECRET="FiRIR90V4wBv8TvVlclJn5WPWQIvENCMickfD32T96V2B"
 
-# options=Options()
-# options.binary_location = '/app/.apt/usr/bin/google-chrome'
-# options.add_argument('--headless')
-# driver=webdriver.Chrome(chrome_options=options)
-driver=webdriver.PhantomJS()
-driver.set_window_size(1124, 850)
-driver.implicitly_wait(30)
-driver.get(URL)
-WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "ProductList_item_inner")))
-html=driver.page_source.encode('utf-8')
+app=Flask(__name__)
 
-soup=BeautifulSoup(html, 'html.parser')
-commodities=soup.find_all('a',attrs={'class':'ProductList_item_inner'})
-# print(commodities)
-productID=commodities[random.randint(0,SERIES_RANGE)].get('href')
+@app.route('/')
+def index():
+    # options=Options()
+    # options.binary_location = '/app/.apt/usr/bin/google-chrome'
+    # options.add_argument('--headless')
+    # driver=webdriver.Chrome(chrome_options=options)
+    driver=webdriver.PhantomJS()
+    driver.set_window_size(1124, 850)
+    driver.implicitly_wait(30)
 
-driver.get(URL+productID)
-WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "HitNum")))
-html=driver.page_source.encode('utf-8')
-soup=BeautifulSoup(html, 'html.parser')
-# print(soup)
-# hitCount=soup.find_all('div',attr={'class':'HitNum'})
-# searchResult=soup.find('section',attr={'class':'SearchResult'})
-searchResult=soup.find(text=re.compile(u'件'))
-# print(searchResult.parent.span.text)
-# print(re.match(r'\d+',searchResult.parent.text))
-hitNum=int(searchResult.parent.span.text)
-num=random.randint(0,hitNum-1)
-pageNum=num//10+1
-# print(hitNum,num)
-# print(url+productID+'&page='+str(pageNum))
-driver.get(URL+productID+'&page='+str(pageNum))
-WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "FAQResultList_item")))
-html=driver.page_source.encode('utf-8')
-driver.close()
-driver.quit()
+    ## 直近の SERIES_RANGE 個の商品からランダムに選択し、そのURLを取得
+    driver.get(URL_PTCG+URL_PTCG_RULE)
+    WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "ProductList_item_inner")))
+    html=driver.page_source.encode('utf-8')
+    soup=BeautifulSoup(html, 'html.parser')
+    commodities=soup.find_all('a',attrs={'class':'ProductList_item_inner'})
+    # print(commodities)
+    productID=commodities[random.randint(0,SERIES_RANGE)].get('href')
 
-soup=BeautifulSoup(html, 'html.parser')
-listItems=soup.find_all('li',attrs={'class':'FAQResultList_item'})
-keyCards=listItems[num%10].find_all('a',attrs={'class':'popup-card-detail'})
-for card in keyCards:
-    print(card.string)
-bodys=listItems[num%10].find_all('div',attrs={'class':'BodyArea'})
-for body in bodys:
-    print(body.text)
+    ## Q&Aの件数を取得
+    driver.get(URL_PTCG+URL_PTCG_RULE+productID)
+    WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "HitNum")))
+    html=driver.page_source.encode('utf-8')
+    soup=BeautifulSoup(html, 'html.parser')
+    # print(soup)
+    # hitCount=soup.find_all('div',attr={'class':'HitNum'})
+    # searchResult=soup.find('section',attr={'class':'SearchResult'})
+    searchResult=soup.find(text=re.compile(u'件'))
+    # print(searchResult.parent.span.text)
+    # print(re.match(r'\d+',searchResult.parent.text))
+    hitNum=int(searchResult.parent.span.text)
+    num=random.randint(0,hitNum-1)
+    pageNum=num//10+1
+    # print(hitNum,num)
+    # print(url+productID+'&page='+str(pageNum))
+
+    ## Q&Aをランダムに取得
+    driver.get(URL_PTCG+URL_PTCG_RULE+productID+'&page='+str(pageNum))
+    WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "FAQResultList_item")))
+    html=driver.page_source.encode('utf-8')
+    soup=BeautifulSoup(html, 'html.parser')
+    listItems=soup.find_all('li',attrs={'class':'FAQResultList_item'})
+    bodys=listItems[num%10].find_all('div',attrs={'class':'BodyArea'})
+    # for body in bodys:
+    #     print(body.text)
+    keyCards=listItems[num%10].find_all('a',attrs={'class':'popup-card-detail'})
+    # for card in keyCards:
+    #     print(card.string,card.get('href'))
+
+    ## 関連カード画像を取得
+    imagePaths=['' for _ in keyCards]
+    imgCnt=0
+    for card in keyCards:
+        driver.get(URL_PTCG+card.get('href'))
+        WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "fit")))
+        html=driver.page_source.encode('utf-8')
+        soup=BeautifulSoup(html, 'html.parser')
+        img=soup.find('img',attrs={'class':'fit'})
+
+        data=urllib.request.urlopen(URL_PTCG+img.get('src')).read()
+        imagePaths[imgCnt]='./'+str(imgCnt)+'.jpg'
+        with open(imagePaths[imgCnt],mode="wb") as f:
+            f.write(data)
+        imgCnt+=1
+
+    driver.close()
+    driver.quit()
+
+    ## リプツリーの形でツイート
+    mySession=OAuth1Session(CONSUMER_KEY,CONSUMER_SECRET,TOKEN,TOKEN_SECRET)
+
+    ## 画像のアップロード
+    mediaIDs=['' for _ in range(imgCnt//4+1)]
+    for i in range(imgCnt):
+        files={'media':open(imagePaths[i],'rb')}
+        req_media=mySession.post(URL_MEDIA,files=files)
+        if req_media.status_code!=200:
+            # print('update error:'+req_media.text)
+            return -1
+        # mediaID=json.loads(req_media.text)['media_id']
+        mediaIDString=json.loads(req_media.text)['media_id_string']
+        if mediaIDs[i//4]=='':
+            mediaIDs[i//4]=mediaIDString
+        else:
+            mediaIDs[i//4]=mediaIDs[i//4]+','+mediaIDString
+
+    ## Questionのツイート
+    status='Q '+bodys[0].text
+    isReply=False
+    tweetCnt=0
+    while True:
+        params={'status':status[:140]}
+        if isReply:
+            req_tl=mySession.get(URL_TL)
+            if req_tl.status_code!=200:
+                # print('time line error'+req_tl.text)
+                return -1
+            id=json.loads(req_tl.text)[0]['id']
+            params['in_reply_to_status_id']=id
+        if imgCnt//4>=tweetCnt:
+            params['media_ids']=mediaIDs[tweetCnt]
+        req_text=mySession.post(URL_TEXT,params=params)
+        if req_text.status_code!=200:
+            # print('tweet error:'+req_text.text)
+            return -1
+
+        tweetCnt+=1
+        if len(status)<=140:
+            if imgCnt//4>=tweetCnt:
+                status='カードの表示'
+            else:
+                break
+        else:
+            status=status[140:]
+        isReply=True
+
+    ## Answerのツイート
+    status='A '+bodys[1].text
+    while True:
+        params={'status':status[:140]}
+        req_tl=mySession.get(URL_TL)
+        if req_tl.status_code!=200:
+            # print('timeline error'+req_tl.text)
+            return -1
+        id=json.loads(req_tl.text)[0]['id']
+        params['in_reply_to_status_id']=id
+        req_text=mySession.post(URL_TEXT,params=params)
+        if req_text.status_code!=200:
+            # print('tweet error:'+req_text.text)
+            return -1
+        if len(status)<=140:
+            break
+        status=status[140:]
+
+    return 0
+
+## おまけ タイムラインの取得
+# req_tl=mySession.get(URL_TL)
+# if req_tl.status_code!=200:
+#     print('time line error'+req_tl.text)
+# timelines=json.loads(req_tl.text)
+# for timeline in timelines:
+#     print(timeline['id'],timeline['text'])
 
 
+## use tweepy
+# auth=tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+# auth.set_access_token(TOKEN,TOKEN_SECRET)
+# t=tweepy.API(auth)
+#
+# t.update_with_media(filename=imagePaths[0],status='test')
+#
+# status='Q '+bodys[0].text
+# isReply=False
+# while True:
+#     if isReply:
+#         id=t.home_timeline()[0].id
+#         t.update_status(status=status[:140],in_reply_to_status_id=id)
+#     else:
+#         t.update_status(status=status[:140])
+#     if len(status)<=140:
+#         break
+#     status=status[140:]
+#     isReply=True
+#
+# status='A '+bodys[1].text
+# while True:
+#     id=t.home_timeline()[0].id
+#     t.update_status(status=status[:140],in_reply_to_status_id=id)
+#     if len(status)<=140:
+#         break
+#     status=status[140:]
 
-# auth=twitter.OAuth(consumer_key=CONSUMER_KEY,
-#     consumer_secret=CONSUMER_SECRET,
-#     token=TOKEN,
-#     token_secret=TOKEN_SECRET)
-# t=twitter.Twitter(auth=auth)
-# status="Hello World"
-# t.statuses.update(status=status)
+## おまけ　タイムラインの取得
+# timelines=t.home_timeline()
+# for timeline in timelines:
+#     print('{id}:{text}'.format(id=timeline.id,text=timeline.text))
--- readme.md
+++ readme.md
@@ -1,19 +1,23 @@
-password wg3v578o
-https://developer.twitter.com/en/account/get-started
-
-参考
-https://qiita.com/yokoh9/items/760e432ebd39040d5a0f
-
-https://qiita.com/yuki_bg/items/96a1608aa3f3225386b6
-
-https://torahack.com/python-scraping-auto-login/
-
-https://kuroyagikun.com/python-twitter-bot-heroku/
-
-API key
-0PWItx3y8rT56fHmoksjCD3p8 (API key)
-Oc7v7lETeQdhOb2mnCXd3hiSBlgJ4DlYSADt7uZlBFSo9IRpbB (API secret key)
-
-Access token
-1103483478723584000-WCkBL5Hjf5bB6rZlsbgrSy2LrPLj8I (Access token)
-FiRIR90V4wBv8TvVlclJn5WPWQIvENCMickfD32T96V2B (Access token secret
+- tweet developer公式   
+  https://developer.twitter.com/en/account/get-started    
+  password wg3v578o
+  - API key   
+    0PWItx3y8rT56fHmoksjCD3p8 (API key)   
+    Oc7v7lETeQdhOb2mnCXd3hiSBlgJ4DlYSADt7uZlBFSo9IRpbB (API secret key)     
+  - Access token    
+    1103483478723584000-WCkBL5Hjf5bB6rZlsbgrSy2LrPLj8I (Access token)   
+    FiRIR90V4wBv8TvVlclJn5WPWQIvENCMickfD32T96V2B (Access token secret)    
+- developerへの登録   
+  https://qiita.com/yokoh9/items/760e432ebd39040d5a0f   
+- headless Chromeの使い方   
+  https://torahack.com/python-scraping-auto-login/    
+- herokuでの定期実行の方法   
+  https://kuroyagikun.com/python-twitter-bot-heroku/    
+- tweet方法   
+  - twitterライブラリを使う   
+    https://qiita.com/yuki_bg/items/96a1608aa3f3225386b6    
+  - ライブラリを使わない    
+    - タイムラインの取得   
+      https://quzee.hatenablog.com/entry/2016/05/08/232539    
+    - 複数画像のツイート   
+      https://qiita.com/sugurunatsuno/items/8899dacbabfab43f6ee8    
