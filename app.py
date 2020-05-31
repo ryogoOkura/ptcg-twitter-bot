ac86cc4338da2564e1dd3750b9cf8e43c48a55b0
Ryogo Okura <nem1442so@gmail.com>
2020-05-31 11:35:23 +0900

    make main func

--- app.py
+++ app.py
@@ -1,145 +1,164 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-WAIT_SECOND=30
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import random
 import re
-SERIES_RANGE=1
-URL_PTCG='https://www.pokemon-card.com/'
-URL_PTCG_RULE='rules/faq/'
 # import urllib.request
 import requests
 import json
 import os
 from requests_oauthlib import OAuth1Session
+
+URL_PTCG='https://www.pokemon-card.com/'
+URL_PTCG_RULE='rules/faq/'
 URL_TEXT='https://api.twitter.com/1.1/statuses/update.json'
 URL_TL='https://api.twitter.com/1.1/statuses/home_timeline.json'
 URL_MEDIA='https://upload.twitter.com/1.1/media/upload.json'
 
-options=Options()
-options.binary_location = '/app/.apt/usr/bin/google-chrome'
-options.add_argument('--headless')
-driver=webdriver.Chrome(chrome_options=options)
-# driver=webdriver.PhantomJS()  # for local
-driver.set_window_size(1124, 850)
-driver.implicitly_wait(30)
+isLocal=False
+if isLocal:
+    import consts
 
-## 直近の SERIES_RANGE 個の商品からランダムに選択し、そのURLを取得
-driver.get(URL_PTCG+URL_PTCG_RULE)
-WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "ProductList_item_inner")))
-html=driver.page_source.encode('utf-8')
-soup=BeautifulSoup(html, 'html.parser')
-commodities=soup.find_all('a',attrs={'class':'ProductList_item_inner'})
-productID=commodities[random.randint(0,SERIES_RANGE)].get('href')
+def main():
+    ## ウェブドライバ起動
+    if isLocal:
+        driver_path='C:\driver\chromedriver_win32\chromedriver'
+    else:
+        driver_path='/app/.chromedriver/bin/chromedriver'
+    options=Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-extentions')
+    options.add_argument('--proxy-server="direct://"')
+    options.add_argument('--proxy-bypass-list=*')
+    options.add_argument('--start-maxmized') # windowサイズ最大化
+    driver=webdriver.Chrome(executable_path=driver_path, chrome_options=options)
+    WAIT_SECOND=30 # 最大表示待ち時間
+    # options.binary_location = '/app/.apt/usr/bin/google-chrome'
 
-## Q&Aの件数を取得
-driver.get(URL_PTCG+URL_PTCG_RULE+productID)
-WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "HitNum")))
-html=driver.page_source.encode('utf-8')
-soup=BeautifulSoup(html, 'html.parser')
-# hitCount=soup.find_all('div',attr={'class':'HitNum'})
-# searchResult=soup.find('section',attr={'class':'SearchResult'})
-searchResult=soup.find(text=re.compile(u'件'))
-hitNum=int(searchResult.parent.span.text)
-num=random.randint(0,hitNum-1)
-pageNum=num//10+1
-
-## Q&Aをランダムに取得
-driver.get(URL_PTCG+URL_PTCG_RULE+productID+'&page='+str(pageNum))
-WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "FAQResultList_item")))
-html=driver.page_source.encode('utf-8')
-soup=BeautifulSoup(html, 'html.parser')
-listItems=soup.find_all('li',attrs={'class':'FAQResultList_item'})
-bodys=listItems[num%10].find_all('div',attrs={'class':'BodyArea'})
-keyCards=listItems[num%10].find_all('a',attrs={'class':'popup-card-detail'})
-
-## 関連カード画像を取得
-imagePaths=['' for _ in keyCards]
-imgCnt=0
-for card in keyCards:
-    driver.get(URL_PTCG+card.get('href'))
-    WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "fit")))
+    ## 直近の SERIES_RANGE 個の商品からランダムに選択し、そのURLを取得
+    ## "Uncaught TypeError: PTC.setCardDetailPopupWindow is not a function" が出る
+    SERIES_RANGE=1
+    driver.get(URL_PTCG+URL_PTCG_RULE)
+    WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "ProductList_item_inner")))
     html=driver.page_source.encode('utf-8')
     soup=BeautifulSoup(html, 'html.parser')
-    img=soup.find('img',attrs={'class':'fit'})
+    commodities=soup.find_all('a',attrs={'class':'ProductList_item_inner'})
+    productID=commodities[random.randint(0,SERIES_RANGE)].get('href')
 
-    # data=urllib.request.urlopen(URL_PTCG+img.get('src')).read()
-    response=requests.get(URL_PTCG+img.get('src'),stream=True)
-    imagePaths[imgCnt]='./'+str(imgCnt)+'.jpg'
-    with open(imagePaths[imgCnt],mode="wb") as f:
-        # f.write(data)
-        f.write(response.content)
-    imgCnt+=1
+    ## Q&Aの件数を取得し、ランダムに選択
+    driver.get(URL_PTCG+URL_PTCG_RULE+productID)
+    WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "HitNum")))
+    html=driver.page_source.encode('utf-8')
+    soup=BeautifulSoup(html, 'html.parser')
+    # hitCount=soup.find_all('div',attr={'class':'HitNum'})
+    # searchResult=soup.find('section',attr={'class':'SearchResult'})
+    searchResult=soup.find(text=re.compile(u'件'))
+    hitNum=int(searchResult.parent.span.text)
+    num=random.randint(0,hitNum-1)
+    pageNum=num//10+1
 
-driver.close()
-driver.quit()
+    ## Q&Aをランダムに取得
+    ## "Uncaught TypeError: PTC.setCardDetailPopupWindow is not a function" が出る
+    driver.get(URL_PTCG+URL_PTCG_RULE+productID+'&page='+str(pageNum))
+    WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "FAQResultList_item")))
+    html=driver.page_source.encode('utf-8')
+    soup=BeautifulSoup(html, 'html.parser')
+    listItems=soup.find_all('li',attrs={'class':'FAQResultList_item'})
+    bodys=listItems[num%10].find_all('div',attrs={'class':'BodyArea'})
+    keyCards=listItems[num%10].find_all('a',attrs={'class':'popup-card-detail'})
 
-## リプツリーの形でツイートしていく
-mySession=OAuth1Session(os.environ['CONSUMER_KEY'],os.environ['CONSUMER_SECRET'],os.environ['TOKEN'],os.environ['TOKEN_SECRET'])
-# from .consts import CONSUMER_KEY,CONSUMER_SECRET,TOKEN,TOKEN_SECRET  # for local
-# mySession=OAuth1Session(CONSUMER_KEY,CONSUMER_SECRET,TOKEN,TOKEN_SECRET)  # for local
+    ## 関連カード画像を取得
+    imagePaths=['' for _ in keyCards]
+    imgCnt=0
+    for card in keyCards:
+        driver.get(URL_PTCG+card.get('href'))
+        WebDriverWait(driver,WAIT_SECOND).until(EC.presence_of_element_located((By.CLASS_NAME, "fit")))
+        html=driver.page_source.encode('utf-8')
+        soup=BeautifulSoup(html, 'html.parser')
+        img=soup.find('img',attrs={'class':'fit'})
 
+        # data=urllib.request.urlopen(URL_PTCG+img.get('src')).read()
+        response=requests.get(URL_PTCG+img.get('src'),stream=True)
+        imagePaths[imgCnt]='./'+str(imgCnt)+'.jpg'
+        with open(imagePaths[imgCnt],mode="wb") as f:
+            # f.write(data)
+            f.write(response.content)
+        imgCnt+=1
 
-## 画像のアップロード
-mediaIDs=['' for _ in range(imgCnt//4+1)]
-for i in range(imgCnt):
-    files={'media':open(imagePaths[i],'rb')}
-    req_media=mySession.post(URL_MEDIA,files=files)
-    # if req_media.status_code!=200:
-    #     print('update error:'+req_media.text)
-    # mediaID=json.loads(req_media.text)['media_id']
-    mediaIDString=json.loads(req_media.text)['media_id_string']
-    if mediaIDs[i//4]=='':
-        mediaIDs[i//4]=mediaIDString
+    driver.quit()
+
+    ## リプツリーの形でツイートしていく
+    ## セッション開始
+    if isLocal:
+        mySession=OAuth1Session(consts.CONSUMER_KEY, consts.CONSUMER_SECRET, consts.TOKEN, consts.TOKEN_SECRET)
     else:
-        mediaIDs[i//4]=mediaIDs[i//4]+','+mediaIDString
+        mySession=OAuth1Session(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'], os.environ['TOKEN'], os.environ['TOKEN_SECRET'])
 
-## Questionのツイート
-status='Q '+bodys[0].text
-isReply=False
-tweetCnt=0
-while True:
-    params={'status':status[:140]}
-    if isReply:
+    ## 画像のアップロード
+    mediaIDs=['' for _ in range(imgCnt//4+1)]
+    for i in range(imgCnt):
+        files={'media':open(imagePaths[i],'rb')}
+        req_media=mySession.post(URL_MEDIA,files=files)
+        # if req_media.status_code!=200:
+        #     print('update error:'+req_media.text)
+        # mediaID=json.loads(req_media.text)['media_id']
+        mediaIDString=json.loads(req_media.text)['media_id_string']
+        if mediaIDs[i//4]=='':
+            mediaIDs[i//4]=mediaIDString
+        else:
+            mediaIDs[i//4]=mediaIDs[i//4]+','+mediaIDString
+
+    ## Questionのツイート
+    status='Q '+bodys[0].text
+    isReply=False
+    tweetCnt=0
+    while True:
+        params={'status':status[:140]}
+        if isReply:
+            req_tl=mySession.get(URL_TL)
+            # if req_tl.status_code!=200:
+            #     print('time line error'+req_tl.text)
+            id=json.loads(req_tl.text)[0]['id']
+            params['in_reply_to_status_id']=id
+        if imgCnt//4>=tweetCnt:
+            params['media_ids']=mediaIDs[tweetCnt]
+        req_text=mySession.post(URL_TEXT,params=params)
+        # if req_text.status_code!=200:
+        #     print('tweet error:'+req_text.text)
+
+        tweetCnt+=1
+        if len(status)<=140:
+            if imgCnt//4>=tweetCnt:
+                status='カードの表示'
+            else:
+                break
+        else:
+            status=status[140:]
+        isReply=True
+
+    ## Answerのツイート
+    status='A '+bodys[1].text
+    while True:
+        params={'status':status[:140]}
         req_tl=mySession.get(URL_TL)
         # if req_tl.status_code!=200:
-        #     print('time line error'+req_tl.text)
+        #     print('timeline error'+req_tl.text)
         id=json.loads(req_tl.text)[0]['id']
         params['in_reply_to_status_id']=id
-    if imgCnt//4>=tweetCnt:
-        params['media_ids']=mediaIDs[tweetCnt]
-    req_text=mySession.post(URL_TEXT,params=params)
-    # if req_text.status_code!=200:
-    #     print('tweet error:'+req_text.text)
-
-    tweetCnt+=1
-    if len(status)<=140:
-        if imgCnt//4>=tweetCnt:
-            status='カードの表示'
-        else:
+        req_text=mySession.post(URL_TEXT,params=params)
+        # if req_text.status_code!=200:
+        #     print('tweet error:'+req_text.text)
+        if len(status)<=140:
             break
-    else:
         status=status[140:]
-    isReply=True
 
-## Answerのツイート
-status='A '+bodys[1].text
-while True:
-    params={'status':status[:140]}
-    req_tl=mySession.get(URL_TL)
-    # if req_tl.status_code!=200:
-    #     print('timeline error'+req_tl.text)
-    id=json.loads(req_tl.text)[0]['id']
-    params['in_reply_to_status_id']=id
-    req_text=mySession.post(URL_TEXT,params=params)
-    # if req_text.status_code!=200:
-    #     print('tweet error:'+req_text.text)
-    if len(status)<=140:
-        break
-    status=status[140:]
+if __name__=='__main__':
+    main()
+
 
 ## おまけ タイムラインの取得
 # req_tl=mySession.get(URL_TL)
